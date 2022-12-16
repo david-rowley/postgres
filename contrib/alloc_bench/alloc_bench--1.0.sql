a8efde8ef00a5658a4cb332220748fe363c0e45b
David Rowley <dgrowley@gmail.com>
2022-12-16 16:59:51 +1300

    WIP

--- contrib/alloc_bench/alloc_bench--1.0.sql
+++ contrib/alloc_bench/alloc_bench--1.0.sql
@@ -4,6 +4,6 @@
 \echo Use "CREATE EXTENSION alloc_bench" to load this file. \quit
 
 CREATE FUNCTION alloc_bench(context_type text, pattern text, nchunks bigint, block_size bigint, chunk_size bigint, nloops int, out mem_allocated bigint, out alloc_ms bigint, out free_ms bigint)
-AS 'MODULE_PATHNAME', 'alloc_bench_random'
+AS 'MODULE_PATHNAME', 'alloc_bench'
 LANGUAGE C VOLATILE STRICT;
 
