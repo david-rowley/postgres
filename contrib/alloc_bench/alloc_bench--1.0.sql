fa9af327c7ae097ade87e48ef402c84a681bdbdf
David Rowley <dgrowley@gmail.com>
2022-12-16 17:02:43 +1300

    WIP

--- contrib/alloc_bench/alloc_bench--1.0.sql
+++ contrib/alloc_bench/alloc_bench--1.0.sql
@@ -3,7 +3,7 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION alloc_bench" to load this file. \quit
 
-CREATE FUNCTION alloc_bench(context_type text, pattern text, nchunks bigint, block_size bigint, chunk_size bigint, nloops int, out mem_allocated bigint, out alloc_ms bigint, out free_ms bigint)
+CREATE FUNCTION alloc_bench(context_type text, pattern text, nchunks bigint, block_size bigint, chunk_size bigint, nloops int, out mem_allocated bigint, out alloc_us bigint, out free_us bigint)
 AS 'MODULE_PATHNAME', 'alloc_bench'
 LANGUAGE C VOLATILE STRICT;
 
