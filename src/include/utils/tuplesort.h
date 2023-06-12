4b5c97bf0804d3d8e346ff4ade9a9f6cfbf7b8fd
David Rowley <dgrowley@gmail.com>
2023-06-13 09:58:48 +1200

    WIP remove srctape and isnull1 from SortTuple

--- src/include/utils/tuplesort.h
+++ src/include/utils/tuplesort.h
@@ -105,42 +105,50 @@ typedef struct TuplesortInstrumentation
 } TuplesortInstrumentation;
 
 /*
- * The objects we actually sort are SortTuple structs.  These contain
- * a pointer to the tuple proper (might be a MinimalTuple or IndexTuple),
- * which is a separate palloc chunk --- we assume it is just one chunk and
- * can be freed by a simple pfree() (except during merge, when we use a
- * simple slab allocator and when performing a non-bounded sort where we
- * use a bump allocator).  SortTuples also contain the tuple's first key
- * column in Datum/nullflag format, and a source/input tape number that
- * tracks which tape each heap element/slot belongs to during merging.
+ * The following two struct definitions are what we actually perform the sort
+ * upon.  We most commonly use SortTuple.  These are what we normally pass to
+ * our sort implementation.  SortTuple store both the tuple and also a Datum
+ * value.  The Datum value duplicates the first sort column's value for
+ * by-val column types.  This allows the value of the first column to be
+ * accessed without having to repeatedly deform the tuple each time we must
+ * compare two tuples.  We'll only need to perform tuple deformation when the
+ * comparison finds the first columns are equal.  We could provide storage for
+ * more than 1 column, but it's likely that the majority of comparisons won't
+ * require comparison of the 2nd column.  If we were to provide space for
+ * further columns, it's likely to cause slowdowns from the additional memory
+ * requirements.
  *
- * Storing the first key column lets us save heap_getattr or index_getattr
- * calls during tuple comparisons.  We could extract and save all the key
- * columns not just the first, but this would increase code complexity and
- * overhead, and wouldn't actually save any comparison cycles in the common
- * case where the first key determines the comparison result.  Note that
- * for a pass-by-reference datatype, datum1 points into the "tuple" storage.
+ * For tuples where the first column value is NULL, we store these in a
+ * separate array in Tuplesortstate.  We will sort the 'nulltuples' array by
+ * any remaining sort columns  at the same time we we sort the 'memtuples'.
+ * The array which we output first depends on NULLS FIRST / NULLS LAST.
  *
- * There is one special case: when the sort support infrastructure provides an
- * "abbreviated key" representation, where the key is (typically) a pass by
- * value proxy for a pass by reference type.  In this case, the abbreviated key
- * is stored in datum1 in place of the actual first key column.
+ * The tuples pointed to by SortTuple and the tuples stored in Tuplesortstate
+ * 'nulltuples' array might be a MinimalTuple or IndexTuple, which is a
+ * separate palloc'd chunk --- we assume it is just one chunk and can be freed
+ * by a simple pfree() (except during merge, when we use a simple slab
+ * allocator and when performing a non-bounded sort where we use a bump
+ * allocator).
  *
- * When sorting single Datums, the data value is represented directly by
- * datum1/isnull1 for pass by value types (or null values).  If the datatype is
- * pass-by-reference and isnull1 is false, then "tuple" points to a separately
- * palloc'd data value, otherwise "tuple" is NULL.  The value of datum1 is then
- * either the same pointer as "tuple", or is an abbreviated key value as
- * described above.  Accordingly, "tuple" is always used in preference to
- * datum1 as the authoritative value for pass-by-reference cases.
+ * When we're unable to perform the entire sort in memory, we perform the sort
+ * in batches.  When Tuplesortstate.availMem is too low to store another
+ * tuple, we perform the sort, then write out those tuples to tape.  Multiple
+ * tapes are compared while outputting the tuples by using a k-way merge
+ * algorithm.  When we read the tuples back from take, we store these in a
+ * DiskTuple.  This is done as we need to record which tape index the given
+ * tuple came from.
  */
 typedef struct
 {
 	void	   *tuple;			/* the tuple itself */
 	Datum		datum1;			/* value of first key column */
-	bool		isnull1;		/* is first key column NULL? */
-	int			srctape;		/* source tape number */
 } SortTuple;
+
+typedef struct
+{
+	void	   *tuple;			/* the tuple itself */
+	int			srctape;		/* source tape number */
+} DiskSortTuple;
 
 typedef int (*SortTupleComparator) (const SortTuple *a, const SortTuple *b,
 									Tuplesortstate *state);
